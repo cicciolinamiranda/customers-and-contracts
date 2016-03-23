ed82938da69eaa82c3fa06b98b93d2368f6a5f63
Cicciolina Miranda <cicciolina.miranda@cloudsherpas>
2016-03-23 22:15:10 +0800

    JP-4371: create MasterService

---
+++ src/main/java/com/g4s/javelin/data/service/location/MasterFileService.java
@@ -0,0 +1,19 @@
+package com.g4s.javelin.data.service.location;
+
+import java.util.List;
+
+import com.g4s.javelin.dto.core.location.EquipmentDTO;
+import com.g4s.javelin.dto.core.location.ModeTransportDTO;
+import com.g4s.javelin.dto.core.location.SkillsDTO;
+import com.g4s.javelin.dto.core.location.TaskDTO;
+
+public interface MasterFileService {
+
+	List<EquipmentDTO> getAllEquipments ();
+
+	List<ModeTransportDTO> getAllModeTransport ();
+
+	List<SkillsDTO> getAllSkills ();
+
+	List<TaskDTO> getAllTasks ();
+}
---
+++ src/main/java/com/g4s/javelin/data/service/location/impl/MasterFileServiceImpl.java
@@ -0,0 +1,37 @@
+package com.g4s.javelin.data.service.location.impl;
+
+import java.util.List;
+
+import com.g4s.javelin.data.service.location.MasterFileService;
+import com.g4s.javelin.dto.core.location.EquipmentDTO;
+import com.g4s.javelin.dto.core.location.ModeTransportDTO;
+import com.g4s.javelin.dto.core.location.SkillsDTO;
+import com.g4s.javelin.dto.core.location.TaskDTO;
+
+public class MasterFileServiceImpl implements MasterFileService {
+
+	@Override
+	public List<EquipmentDTO> getAllEquipments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ModeTransportDTO> getAllModeTransport() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SkillsDTO> getAllSkills() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TaskDTO> getAllTasks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
