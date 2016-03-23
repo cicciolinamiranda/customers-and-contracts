a692d89446a619bf7241d0a52266dc5fdfa96913
Cicciolina Miranda <cicciolina.miranda@cloudsherpas>
2016-03-23 22:23:23 +0800

    JP-4372: added properties to TaskDTO and added QueryResponseDTO for lists

--- src/main/java/com/g4s/javelin/dto/core/location/BaseListResponseDTO.java
+++ src/main/java/com/g4s/javelin/dto/core/location/BaseListResponseDTO.java
@@ -2,20 +2,13 @@ package com.g4s.javelin.dto.core.location;
 
 import java.util.List;
 
-public class QueryResponseDTO<T> {
+public class BaseListResponseDTO<T> {
 
     private List<T> list;
-    private String cursor;
     public  List<T> getList() {
         return list;
     }
     public void setList(final  List<T> list) {
         this.list = list;
-    }
-    public String getCursor() {
-        return cursor;
-    }
-    public void setCursor(final String cursor) {
-        this.cursor = cursor;
     }
 }
