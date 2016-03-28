c0f33ae3c9ab506c31dddc505bf2e2d2f4f48622
Jordan Duabe <jordan.duabe@cloudsherpas.com>
2016-03-28 20:31:07 +0800

    Add work-order root level api constants
    - Renamed ResourceConstants.java to ApiConstants.java
    
    JP-4374

---
+++ src/main/java/com/g4s/javelin/constants/ApiConstants.java
@@ -0,0 +1,22 @@
+package com.g4s.javelin.constants;
+
+/**
+ * @author Jordan Duabe
+ * @since 03/28/2016
+ */
+public final class ApiConstants {
+
+    public static final String API_NAME = "work-order";
+
+    public static final String API_VERSION = "v1";
+
+    public static final String API_NAMESPACE_OWNER_DOMAIN = "Accenture";
+
+    public static final String API_NAMESPACE_OWNER_NAME = "accenture.com";
+
+    public static final String API_DESCRIPTION = "G4S Javelin Location API ";
+
+    private ApiConstants() {
+
+    }
+}
