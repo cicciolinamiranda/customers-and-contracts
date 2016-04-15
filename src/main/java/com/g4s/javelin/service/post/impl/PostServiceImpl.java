5a46ee7288aa73122aee9f4fceb13b2a557685b7
Sharlyn Mae Pandes <sharlyn.pandes@cloudsherpas.com>
2016-04-15 21:06:37 +0800

    Fix for Gender

--- src/main/java/com/g4s/javelin/service/post/impl/PostServiceImpl.java
+++ src/main/java/com/g4s/javelin/service/post/impl/PostServiceImpl.java
@@ -99,9 +99,11 @@ public class PostServiceImpl implements PostService {
     public PostDTO getPostDetails(final Long id) {
         PostDTO dto = new PostDTO();
         PostModel result = postRepository.findOne(id);
+
         if (result != null) {
             dto = transformPostModel(result);
         }
+
         return dto;
     }
 
@@ -191,6 +193,10 @@ public class PostServiceImpl implements PostService {
                     .getQualifications()));
             model.setReligions(transformMasterfileDTO(dto.getReligions()));
             model.setTrainings(transformMasterfileDTO(dto.getTrainings()));
+            if (dto.getGender() != null) {
+                model.setGender(modelMapper.map(dto.getGender(),
+                        MasterfileModel.class));
+            }
         }
         return model;
     }
