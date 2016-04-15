42985597127cf6e4324f582cbaebbd88da9a0f5e
jcaronan <jasmin.caronan@gmail.com>
2016-04-15 21:12:13 +0800

    JP-4690 JP-2306
     Modfy Customer DTO to add the ff fields:
      - customerNumber, customerCode,manualCustomerCode,vatNumber,dunsNumber,addressLine1,
        city, tbc, accountNumber, accountDescription

--- src/main/java/com/g4s/javelin/dto/core/location/CustomerDTO.java
+++ src/main/java/com/g4s/javelin/dto/core/location/CustomerDTO.java
@@ -6,6 +6,26 @@ public class CustomerDTO extends BaseDTO {
 
     private String customerName;
 
+    private String customerNumber;
+
+    private String customerCode;
+
+    private String dunsNumber;
+
+    private String accountNumber;
+
+    private String accountDescription;
+
+    private String manualCustomerCode;
+
+    private String vatNumber;
+
+    private String addressLine1;
+
+    private String city;
+
+    private String tbc;
+
     public String getCustomerName() {
         return customerName;
     }
@@ -14,4 +34,83 @@ public class CustomerDTO extends BaseDTO {
         this.customerName = customerName;
     }
 
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(final String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(final String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getDunsNumber() {
+        return dunsNumber;
+    }
+
+    public void setDunsNumber(final String dunsNumber) {
+        this.dunsNumber = dunsNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(final String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountDescription() {
+        return accountDescription;
+    }
+
+    public void setAccountDescription(final String accountDescription) {
+        this.accountDescription = accountDescription;
+    }
+
+    public String getManualCustomerCode() {
+        return manualCustomerCode;
+    }
+
+    public void setManualCustomerCode(final String manualCustomerCode) {
+        this.manualCustomerCode = manualCustomerCode;
+    }
+
+    public String getVatNumber() {
+        return vatNumber;
+    }
+
+    public void setVatNumber(final String vatNumber) {
+        this.vatNumber = vatNumber;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(final String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public String getTbc() {
+        return tbc;
+    }
+
+    public void setTbc(final String tbc) {
+        this.tbc = tbc;
+    }
 }
