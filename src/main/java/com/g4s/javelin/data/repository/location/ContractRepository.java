0f3c91117137d3b0a0b7e0dfa32c3c00ee79ff36
jcaronan <jasmin.caronan@gmail.com>
2016-04-16 17:02:41 +0800

    JP-2312 JP-2311
    Create contract repository to add and find contracts.

---
+++ src/main/java/com/g4s/javelin/data/repository/location/ContractRepository.java
@@ -0,0 +1,28 @@
+package com.g4s.javelin.data.repository.location;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.g4s.javelin.data.model.location.ContractModel;
+
+/**
+ * Created by apadilla on 4/8/16.
+ */
+public interface ContractRepository extends JpaRepository<ContractModel, Long> {
+
+    List<ContractModel> findAll();
+
+    List<ContractModel> findByCustomerNumber(final String customerNumber);
+
+    List<ContractModel> findByNumber(final String number);
+
+    List<ContractModel> findByName(final String name);
+
+    @Query("SELECT CM FROM ContractModel CM where CM.id = ?1 OR CM.number LIKE %?2% "
+            + "OR CM.name LIKE %?2% OR CM.title LIKE %?2%")
+    List<ContractModel> findBySearchTerm(Long id, String customerName);
+
+
+}
