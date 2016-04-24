47f1cd7eca491dfde4380c20605dd76fb121bb9f
darryl.ong <darryl.ong@cloudsherpas.com>
2016-04-25 07:07:30 +0800

    Improved Diff Capture, requires change in frontend

--- src/main/java/com/g4s/javelin/util/DiffUtil.java
+++ src/main/java/com/g4s/javelin/util/DiffUtil.java
@@ -5,12 +5,18 @@ import java.util.List;
 import org.javers.core.Javers;
 import org.javers.core.JaversBuilder;
 import org.javers.core.diff.Diff;
+import org.javers.core.diff.changetype.PropertyChange;
+import org.javers.core.diff.changetype.ReferenceChange;
 import org.javers.core.diff.changetype.ValueChange;
 
 import com.g4s.javelin.dto.core.audit.DiffDTO;
 import com.google.common.collect.Lists;
+import org.javers.core.diff.changetype.container.ContainerChange;
+import org.javers.core.diff.changetype.container.ContainerElementChange;
+import org.javers.core.diff.changetype.container.ValueAdded;
+import org.javers.core.diff.changetype.container.ValueRemoved;
 
-
+//CSOFF: CyclomaticComplexity
 public class DiffUtil {
 
     protected List<DiffDTO> getOldAndNewValue(
@@ -19,29 +25,52 @@ public class DiffUtil {
         List<DiffDTO> response = Lists.newArrayList();
         Javers javers = JaversBuilder.javers().build();
         Diff diff = javers.compare(oldValue, newValue);
-        for (int i = 1; i < diff.getChangesByType(ValueChange.class).size(); i++) {
-            ValueChange change = diff.getChangesByType(ValueChange.class)
-                    .get(i);
+        List<PropertyChange> changes = diff.getChangesByType(PropertyChange.class);
+        for (int i = 1; i < changes.size(); i++) {
+            PropertyChange change = changes.get(i);
+
             DiffDTO diffDTO = new DiffDTO();
             diffDTO.setFieldName(change.getPropertyName());
-            //change.getLeft().toString();
-            List<String> oldValues = Lists.newArrayList();
-            if (null == change.getLeft()) {
-                oldValues.add("");
-            } else {
-                oldValues.add(change.getLeft().toString());
+            if (change instanceof ContainerChange) {
+                for (ContainerElementChange entry : ((ContainerChange) change).getChanges()) {
+                    if (entry instanceof ValueAdded) {
+                        diffDTO.getChanges().add("Added #" + formatPropertyValue(entry.toString()));
+                    } else if (entry instanceof ValueRemoved) {
+                        diffDTO.getChanges().add("Removed #" + formatPropertyValue(entry.toString()));
+                    }
+                }
+            } else if (change instanceof ReferenceChange) {
+                ReferenceChange refChange = (ReferenceChange) change;
+                String leftChange = refChange.getLeft() == null ? "" : refChange.getLeft().toString();
+                String rightChange = refChange.getRight() == null ? "" : refChange.getRight().toString();
+
+                diffDTO.getChanges().add(formatPropertyValue(leftChange)
+                        + " -> " + formatPropertyValue(rightChange));
+            } else if (change instanceof ValueChange) {
+                ValueChange valChange = (ValueChange) change;
+                change.getPropertyName()
+                diffDTO.getChanges().add(valChange.getLeft() + " -> " + valChange.getRight());
             }
-            diffDTO.setOldValue(oldValues);
-            List<String> newValues = Lists.newArrayList();
-            if (null == change.getRight()) {
-                newValues.add("");
-            } else {
-                newValues.add(change.getRight().toString());
-            }
-            diffDTO.setNewValue(newValues);
+
             response.add(diffDTO);
         }
         return response;
     }
 
+    protected String formatPropertyValue(final String propertyValue) {
+        String formatted = null;
+
+        if (propertyValue == null) {
+            formatted = "None";
+        } else if (propertyValue.lastIndexOf("/") > -1) {
+            formatted = propertyValue.replaceAll("^'|'$", "")
+                    .substring(propertyValue.lastIndexOf("/") + 1).trim();
+        } else {
+            formatted = propertyValue;
+        }
+
+        return formatted;
+    }
+
 }
+//CSON: CyclomaticComplexity
